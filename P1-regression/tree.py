5ba4092beab5278426d84a6465dec8a68e66ef97
wfawwer <31596190+wfawwer@users.noreply.github.com>
2018-01-31 23:09:10 -0500

    add features

---
+++ P1-regression/tree.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
+
+
+print("xd")
+df_train = pd.read_csv("train.csv")
+df_test = pd.read_csv("test.csv")
+
+
+#store gap values
+Y_train = df_train.gap.values
+#row where testing examples start
+test_idx = df_train.shape[0]
+#delete 'Id' column
+df_test = df_test.drop(['Id'], axis=1)
+#delete 'gap' column
+df_train = df_train.drop(['gap'], axis=1)'
+
+
+
+## feature engineering here
+
+
+
+#df_all = df_all.drop(['smiles'], axis=1)
+#vals = df_all.values
+#X_train = vals[:test_idx]
+#X_test = vals[test_idx:]
+
+#RF = RandomForestRegressor()
+#RF.fit(X_train, Y_train)
+#RF_pred = RF.predict(X_test)
+
+#def write_to_file(filename, predictions):
+#    with open(filename, "w") as f:
+#        f.write("Id,Prediction\n")
+#        for i,p in enumerate(predictions):
+#            f.write(str(i+1) + "," + str(p) + "\n")
+
+
+#write_to_file("sample2.csv", RF_pred)
