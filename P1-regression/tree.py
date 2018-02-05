0b1668f5ad09f015895a721cab039fe2f123b952
Vinh-Kha Le <vinhkhale@gmail.com>
2018-02-05 01:04:07 -0500

    New code in tree.py

--- P1-regression/tree.py
+++ P1-regression/tree.py
@@ -4,6 +4,9 @@ import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 from sklearn.ensemble import RandomForestRegressor
 from rdkit import Chem
+from rdkit import DataStructs
+from rdkit.Chem.Fingerprints import FingerprintMols
+from rdkit.Chem import MACCSkeys
 
 print("xd")
 df_train = pd.read_csv("train.csv")
@@ -24,10 +27,30 @@ df_train = df_train.drop(['gap'], axis=1)
 print("ok")
 #merge train and data so we can engineer features
 df_all = pd.concat((df_train, df_test), axis=0)
-
-
-SSSR_len = np.vstack(df_all.smiles.astype(str).apply(lambda x: Chem.GetSSSR(x)))
+df_all = df_all[:1000] # comment out for full data set
+mol_all = [Chem.MolFromSmiles(x) for x in df_all.smiles.astype(str)]
+SSSR_len = np.vstack([Chem.GetSSSR(x) for x in mol_all])
 print(SSSR_len)
+alkyl_smarts = "[CX4]"
+alkyl = Chem.MolFromSmarts(alkyl_smarts)
+alkyl_count = np.vstack([len(Chem.Mol.GetSubstructMatches(x, alkyl, uniquify = True)) for x in mol_all])
+print(alkyl_count)
+fps = [FingerprintMols.FingerprintMol(x) for x in mol_all]
+maccs = [MACCSkeys.GenMACCSKeys(x) for x in mol_all]
+co2 = Chem.MolFromSmiles('C(=O)=O')
+fps_co2 = FingerprintMols.FingerprintMol(co2)
+fps_co2_sim = np.vstack([DataStructs.FingerprintSimilarity(x, fps_co2) for x in fps])
+#fps_co2_sim = np.vstack([DataStructs.FingerprintSimilarity(x, fps_co2, metric=DataStructs.DiceSimilarity) for x in fps])
+#CosineSimilarity, SokalSimilarity, RusselSimilarity, RogotGoldbergSimilarity
+#AllBitSimilarity, KulczynskiSimilarity, McConnaugheySimilarity, AsymmetricSimilarity, BraunBlanquetSimilarity
+print(fps_co2_sim)
+maccs_co2 = MACCSkeys.GenMACCSKeys(co2)
+maccs_co2_sim = np.vstack([DataStructs.FingerprintSimilarity(x, maccs_co2) for x in maccs])
+#maccs_co2_sim = np.vstack([DataStructs.FingerprintSimilarity(x, maccs_co2, metric=DataStructs.DiceSimilarity) for x in maccs])
+#CosineSimilarity, SokalSimilarity, RusselSimilarity, RogotGoldbergSimilarity
+#AllBitSimilarity, KulczynskiSimilarity, McConnaugheySimilarity, AsymmetricSimilarity, BraunBlanquetSimilarity
+print(maccs_co2_sim)
+print(np.vstack(df_all.smiles.astype(str)))
 #df_all['smiles_len'] = pd.DataFrame(smiles_len)
 
 
